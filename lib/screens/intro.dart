2bda106f6f60812f67badcbb636ceedde4156e28
Muneeb-Ahmad-Ch <muneeb.ahmad.freelancing@gmail.com>
2023-01-18 02:44:19 +0500

    read me updated

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -12,65 +12,67 @@ class _IntroState extends State<Intro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(bottom: 40),
-            child: Image.asset(
-              "assets/images/fruits.png",
-              height: 220,
-              width: 300,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 50,
-              right: 50,
-              bottom: 20,
-            ),
-            child: Text(
-              "Your one-stop destination for all your fruit needs!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w400,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(bottom: 40),
+              child: Image.asset(
+                "assets/images/fruits.png",
+                height: 220,
+                width: 300,
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 50,
-              right: 50,
-              bottom: 80,
-            ),
-            child: Text(
-              "Fresh Items Everyday",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 50,
+                right: 50,
+                bottom: 20,
               ),
-            ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                minimumSize: Size(150, 70),
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0),
+              child: Text(
+                "Your one-stop destination for all your fruit needs!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w400,
                 ),
-                backgroundColor: Colors.deepPurpleAccent),
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) {
-                return Home();
-              }));
-            },
-            child: const Text("Get Started", style: TextStyle(fontSize: 18)),
-          ),
-        ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 50,
+                right: 50,
+                bottom: 80,
+              ),
+              child: Text(
+                "Fresh Items Everyday",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(150, 70),
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  backgroundColor: Colors.deepPurpleAccent),
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
+                  return Home();
+                }));
+              },
+              child: const Text("Get Started", style: TextStyle(fontSize: 18)),
+            ),
+          ],
+        ),
       ),
     );
   }
